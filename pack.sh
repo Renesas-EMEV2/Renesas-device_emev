c4fcb9de47328a83dc02fc3cb2f47e7f4e6c5a39
ffxx68 <ffumi68@gmail.com>
2012-04-20 16:26:11 +0200

    Fixed packaging procedure

--- pack.sh
+++ pack.sh
@@ -4,26 +4,34 @@ ANDROID_DIR=$AOSP
 WORK_DIR=$ANDROID_DIR
 OBJ_DIR=$ANDROID_DIR
 
-#Create Userland
+# Create Userland
 echo ""
 echo "-----------------------"
 echo "- userland deployment -"
 echo "-----------------------"
-cd  ${ANDROID_DIR}
-rm -f tmp-android.tar.gz android-fs.tar.gz
-sudo rm -rf android-fs/
+
+# cleanup
+rm -f ${ANDROID_DIR}/tmp-android.tar.gz 
+rm -f ${OBJ_DIR}/android-fs4.tar.gz
+sudo rm -rf ${WORK_DIR}/android-fs
+
 cd ${ANDROID_DIR}/out/target/product/emev/
-cp ${ANDROID_DIR}/device/renesas/emev/initlogo.rle ./system/
+cp ${ANDROID_DIR}/device/renesas/emev/initlogo.rle ./root/
 tar zcf ${ANDROID_DIR}/tmp-android.tar.gz system data root
-cd ${WORK_DIR}
+
 if [ ! -d ${WORK_DIR}/android-fs ] ; then
-mkdir android-fs
+  cd ${WORK_DIR}
+  mkdir android-fs
 fi
-cd android-fs
+cd ${WORK_DIR}/android-fs
 tar zxf ${ANDROID_DIR}/tmp-android.tar.gz
 mv data root
 mv system root
 mv root/* ./
+
+# Copying builtin firmware
+#cp -r ${ANDROID_DIR}/device/renesas/emev/firmware/* ./
+
 rmdir root
 chmod +r system/usr/keychars/*
 sudo chown -R 1000:1000 ./
---
+++ pull_firmware.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+SGX_FILES=${AOSP}/device/renesas/emev/firmware
+OMF_FILES=${AOSP}/device/renesas/emev/firmware
+
+mkdir -p ${SGX_FILES}/lib/modules
+mkdir -p ${SGX_FILES}/system/etc/
+mkdir -p ${SGX_FILES}/system/lib/egl/
+mkdir -p ${SGX_FILES}/system/vendor/bin/
+mkdir -p ${SGX_FILES}/system/vendor/lib/egl/
+mkdir -p ${SGX_FILES}/system/vendor/lib/hw/
+
+adb pull /lib/modules/bc_example.ko ${SGX_FILES}/lib/modules/
+adb pull /lib/modules/emxxlfb.ko ${SGX_FILES}/lib/modules/
+adb pull /lib/modules/pvrsrvkm.ko ${SGX_FILES}/lib/modules/
+adb pull /system/etc/powervr.ini ${SGX_FILES}/system/etc/
+adb pull /system/lib/egl/egl.cfg ${SGX_FILES}/system/lib/egl/
+adb pull /system/vendor/bin/pvrsrvinit ${SGX_FILES}/system/vendor/bin/
+adb pull /system/vendor/lib/libglslcompiler.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/libIMGegl.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/libpvr2d.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/libpvrANDROID_WSEGL.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/libsrv_init.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/libsrv_um.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/libusc.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/egl/libEGL_POWERVR_SGX530_120.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX530_120.so ${SGX_FILES}/system/vendor/lib/egl/
+adb pull /system/vendor/lib/egl/libGLESv2_POWERVR_SGX530_120.so ${SGX_FILES}/system/vendor/lib/egl/
+#adb pull /system/vendor/lib/hw/gralloc.emxx.so ${SGX_FILES}/system/vendor/lib/hw/
+
+chmod 755 ${SGX_FILES}/system/vendor/bin/pvrsrvinit
+
+mkdir -p ${OMF_FILES}/system/etc/
+mkdir -p ${OMF_FILES}/system/lib/omf/dspfw/
+mkdir -p ${OMF_FILES}/lib/
+mkdir -p ${OMF_FILES}/system/lib/
+
+adb pull /system/etc/pvplayer.cfg ${OMF_FILES}/system/etc/
+adb pull /system/lib/libomf_manager.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/libomf_me_video_cmn.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/omf/dspfw/omf_dsp_manager.em-ev ${OMF_FILES}/system/lib/omf/dspfw/
+adb pull /system/lib/omf/dspfw/omf_me_aacd.em-ev ${OMF_FILES}/system/lib/omf/dspfw/
+adb pull /system/lib/omf/dspfw/omf_me_jpegd.em-ev ${OMF_FILES}/system/lib/omf/dspfw/
+adb pull /system/lib/omf/dspfw/omf_me_jpege.em-ev ${OMF_FILES}/system/lib/omf/dspfw/
+adb pull /system/lib/omf/omx_av_codec.cfg ${OMF_FILES}/system/lib/omf/
+adb pull /system/lib/omf_mc_aacd.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/omf_mc_h264d.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/omf_mc_jpegd.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/omf_mc_jpege.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/omf_mc_m4vd.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/omf_mc_m2vd.so ${OMF_FILES}/system/lib/
+adb pull /system/lib/omf_mc_vc1d.so ${OMF_FILES}/system/lib/
+
+cp ${OMF_FILES}/system/lib/libomf_manager.so ${OMF_FILES}/lib/
