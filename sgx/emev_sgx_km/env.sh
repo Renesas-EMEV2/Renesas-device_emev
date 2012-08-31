b054b75e8fda057870ef84a7a6fc3bf2ec9736c9
ffxx68 <ffumi68@gmail.com>
2012-08-31 07:53:28 -0700

    Fix for SGX modules compiling

--- sgx/emev_sgx_km/env.sh
+++ sgx/emev_sgx_km/env.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
 export LANG=C
 export ARCH=arm
 export KERNELDIR=$KERNEL
 export ANDROID_ROOT=$AOSP
 export CROSS_COMPILE=arm-eabi-
 export KERNEL_CROSS_COMPILE=$CROSS_COMPILE
+cd eurasia_km/eurasiacon/build/linux2/emev_android
+make 
