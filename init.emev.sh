7d1fc49ee3ceaae42e426ca6f73f3969defffe29
Fabio Fumi <ffumi68@gmail.com>
2012-12-11 17:35:08 +0100

    Fixed SGX init script

--- init.emev.sh
+++ init.emev.sh
@@ -5,8 +5,8 @@ chmod 666 /sys/power/sleep_while_idle
 insmod /lib/modules/inter_dsp.ko
 insmod /lib/modules/em_ave.ko
  
-ln -s /dev/v4l/video0 /dev/video0
-ln -s /dev/v4l/video1 /dev/video1
+#ln -s /dev/v4l/video0 /dev/video0
+#ln -s /dev/v4l/video1 /dev/video1
 
 mkdir /dev/fb
 ln -s /dev/graphics/fb0 /dev/fb/0
@@ -16,12 +16,11 @@ ret=$?
 
 case ${ret} in
   0)
-    mv /system/lib/hw/gralloc.emxx.so /system/lib/hw/gralloc.emxx.so_tmp
+#    mv /system/lib/hw/gralloc.emxx.so /system/lib/hw/gralloc.emxx.so_tmp
     insmod /lib/modules/pvrsrvkm.ko
     /vendor/bin/pvrsrvinit
 
     insmod /lib/modules/emxxlfb.ko
-    insmod /lib/modules/bc_example.ko
     ;;
 esac
 
